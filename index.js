1895a687607fce9fcccae1b41bde939f582ef89b
Laged <parkkila.matti@gmail.com>
2016-10-28 16:17:34 +0300

    Fix azure issues

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const port = process.env.PORT | 3000;
-const app = express()
-.get('/', (req, res) => {
-	res.json({status:"up"});
-})
-.listen(process.env.PORT | 3000, () => console.log('Running on ' + port));
