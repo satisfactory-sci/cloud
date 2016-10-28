349990e1bf27e1e99fe55ae915b73a2a058d08bc
Laged <parkkila.matti@gmail.com>
2016-10-28 16:24:44 +0300

    Add public

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const port = process.env.PORT | 3000;
-const app = express()
+const app = express();
+
+app.use(express.static('public'));
+
+app
 .get('/', (req, res) => {
 	res.json({status:"up"});
 })
