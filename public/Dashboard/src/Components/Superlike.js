eafaa8f20ca4fc59a550a31f5e1e7c81d78768cd
Ville Vainio <ville.vainio@aalto.fi>
2016-11-03 21:45:01 +0200

    Stylize SuperLike card

--- public/Dashboard/src/Components/Superlike.js
+++ public/Dashboard/src/Components/Superlike.js
@@ -7,7 +7,9 @@ class SuperLike extends React.Component {
     <div id="toast">
       <div className="card card-block" id="card">
         <img className="card-img-top img-thumbnail" id="toast-img" src={this.props.img} alt="Card image cap"></img>
-          <h4 className="card-title" id="toast-title">{this.props.title} <br />Got MegaLiked <br /> Mega Count: {this.props.count}</h4>
+          <h3 className="card-title" id="toast-title">{this.props.title}</h3>
+          <h4 className="card-title" id="toast-like">Got MegaLiked!</h4>
+          <div className="card-text">Mega Count: {this.props.count}</div>
       </div>
     </div>
   )
